b28851324134b035db1b72856ac277d38bc6ffec
cdoller <carlosoller1994@gmail.com>
2022-11-17 00:32:52 -0300

    bootstrap instalado con npm y carousel funcionando

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,20 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel';
 import iconoMessi from "../assets/statics/messi.png";
+
 
 const Carrousel = (props)=>{
     // hacer deconstruccion de props
 
     return (
-        <div>
-            <h2>Este es el carrousel</h2>
-            <div className="card" style={{width: '200px'}}>
-                <img src={iconoMessi} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-warning">Go somewhere</a>
-                </div>
-            </div>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={iconoMessi}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={iconoMessi}
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={iconoMessi}
+                alt="Third slide"
+                />
+
+            <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+            </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     );
 }
 
