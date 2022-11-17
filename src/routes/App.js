cd2b3e4bc4d13ab6927af295e00e83eae2e4fd7d
cdoller <carlosoller1994@gmail.com>
2022-11-17 00:38:13 -0300

    link de bootstrap cambiado de lugar, ahora esta en index dentro de la carpeta public

--- src/routes/App.js
+++ src/routes/App.js
@@ -5,12 +5,7 @@ import HomePage from '../containers/HomePage';
 function App() {
   return (
     <div className="App">
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-        crossorigin="anonymous"
-      />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
