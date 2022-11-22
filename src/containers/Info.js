583181c8281753d89821410bbfe8b53c10260c0b
florescobarlevi <florenciaescobarlevi@gmail.com>
2022-11-21 21:38:01 -0300

    seccion 2 ok, falta chequear el responsive

--- src/containers/Info.js
+++ src/containers/Info.js
@@ -10,16 +10,17 @@ const Info = () =>{
         <section className='Info'>
             <Container fluid className='Container'>
                 <Row fluid className='Row'>
-                    <Col className='Col1' lg={1}></Col>
-                    <Col className='Col2' lg={2}></Col>
-                    <Col className='Col3' lg={7}>
+                    <Col className='Col1' xs={1} lg={1}></Col>
+                    <Col className='Col2' xs={1} lg={2}></Col>
+                    <Col className='Col3' xs={9} lg={7}>
                         <h2>SECCIÓN INFORMACIÓN</h2>
                         <h2 className="TitleColor">WE HELP PEOPLE</h2>                        
                         <p>Aqui se describe que hacemos, esto es solo un turnero asi que bla blabla</p>                        
                         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
                 
+
                     </Col>
-                    <Col className='Col2' lg={2}></Col>          
+                    <Col className='Col2' xs={1} lg={2}></Col>          
                 </Row>
                           
             </Container>
