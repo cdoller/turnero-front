80d84c3325346d43f1b35e06d6760e360c65c3eb
cdoller <carlosoller1994@gmail.com>
2022-11-23 12:13:59 -0300

    se acomoda el app.js y se quita un componente formulario

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,12 +3,12 @@ import '../assets/css/App.css';
 import HomePage from '../containers/HomePage';
 import Contact from '../containers/Contact';
 import Contact2 from '../containers/Contact2';
+import Info from '../containers/Info';
 
 function App() {
   return (
     <div className="App">
-      <Contact></Contact>
-      <hr></hr>
+      <Info></Info>
       <Contact2></Contact2>
     </div>
   );
