ca7349dcbd29621ad7afc83d74ecd097d8a5d097
cdoller <carlosoller1994@gmail.com>
2022-11-25 20:51:10 -0300

    formulario funcionando con bootstrap

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,7 +1,6 @@
 import logo from '../logo.svg';
 import '../assets/css/App.css';
 import HomePage from '../containers/HomePage';
-import Contact from '../containers/Contact';
 import Contact2 from '../containers/Contact2';
 
 function App() {
