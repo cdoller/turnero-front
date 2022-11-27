092d45e5a685b266ea5daf210167150a90534170
cdoller <carlosoller1994@gmail.com>
2022-11-27 16:30:55 -0300

    acordion modificado

--- src/containers/Pyr.js
+++ src/containers/Pyr.js
@@ -1,12 +1,12 @@
 import Acordion from "../components/Acordion"
 import "../assets/css/Pyr.css"
-import fondoAcordeon from "../assets/statics/fondoAcordeon.png"
-
+import ImagenPreguntas from "../assets/statics/preguntas.webp"
 
 const Pyr = ()=>{
 
     return (
-        <div className="pyr">
+        <div className="pyr position-relative">
+            <img src={ImagenPreguntas} alt='imagen de persona con dudas y consultas' width="800px" className="position-absolute zindex-dropdown start-0"/>
             <Acordion></Acordion>  
         </div>
     )
