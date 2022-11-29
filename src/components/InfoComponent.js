8bcbfd93a0ee01397f7f42fe81ef8b7257131042
florescobarlevi <florenciaescobarlevi@gmail.com>
2022-11-28 21:40:36 -0300

    sass hasta aca ok

--- src/components/InfoComponent.js
+++ src/components/InfoComponent.js
@@ -10,14 +10,14 @@ const InfoComponent = () =>{
         <>
         <section className='Info'>
             <Container fluid className='Container container-info'>
-                <Row fluid className='Row'>
-                    <Col className='Col1' xs={1} lg={1}></Col>
-                    <Col className='Col2' xs={1} lg={2}></Col>
-                    <Col className='Col3' xs={9} lg={7}>
+                <Row fluid className='Row rowInfo'>
+                    <Col className='Col1 columnaColor' xs={1} lg={1}></Col>
+                    <Col className='Col2 columnaBlanca' xs={1} lg={2}></Col>
+                    <Col className='Col3 columnaTexto' xs={9} lg={7}>
                         <h2>SECCIÓN INFORMACIÓN</h2>
-                        <h2 className="TitleColor">WE HELP PEOPLE</h2>                        
-                        <p>Aqui se describe que hacemos, esto es solo un turnero asi que bla blabla</p>                        
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        <h2 className="TitleColor segundoTitle">WE HELP PEOPLE</h2>                        
+                        <p pInfo>Aqui se describe que hacemos, esto es solo un turnero asi que bla blabla</p>                        
+                        <p pInfo>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
                 
 
                     </Col>
